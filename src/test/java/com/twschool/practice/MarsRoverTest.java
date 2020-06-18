b337563d1ac45fe2d1bc9f395f3f591595392b02
luna <1661639144@qq.com>
2020-06-18 20:02:59 +0800

    luna finish should_return_coordinate_xf1_y0_direction_W_given_coordinate_x0_y0_direction_W_command_M

--- src/test/java/com/twschool/practice/MarsRoverTest.java
+++ src/test/java/com/twschool/practice/MarsRoverTest.java
@@ -149,15 +149,15 @@ public class MarsRoverTest {
     }
 
     @Test
-    public void  should_return_coordinate_xf1_y0_direction_E_given_coordinate_x0_y0_direction_E_command_M(){
+    public void  should_return_coordinate_xf1_y0_direction_W_given_coordinate_x0_y0_direction_W_command_M(){
         //given
-        MarsRoverPostion marsRoverPostion =new MarsRoverPostion(0,0,"E");
+        MarsRoverPostion marsRoverPostion =new MarsRoverPostion(0,0,"W");
         MarsRover marsRover =new MarsRover(marsRoverPostion);
         //when
         marsRover.receive("M");
         //then
-        Assert.assertEquals("E",marsRoverPostion.getDirection());
-        Assert.assertEquals(1,marsRoverPostion.getcoordinateX());
+        Assert.assertEquals("W",marsRoverPostion.getDirection());
+        Assert.assertEquals(-1,marsRoverPostion.getcoordinateX());
         Assert.assertEquals(0,marsRoverPostion.getCoordinateY());
     }
 
