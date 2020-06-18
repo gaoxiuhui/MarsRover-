418173cc06d74e2a123a632e25f572332ded6284
luna <1661639144@qq.com>
2020-06-18 18:18:58 +0800

    luna finish should_return_coordinate_x0_y0_direction_E_given_coordinate_x0_y0_direction_S_command_L

--- src/test/java/com/twschool/practice/MarsRoverTest.java
+++ src/test/java/com/twschool/practice/MarsRoverTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class MarsRoverTest {
 
     @Test
-    public void  should_return_coordinate_x0_y0_direction_W_given_coordinate_x0_y0_direction_N(){
+    public void  should_return_coordinate_x0_y0_direction_W_given_coordinate_x0_y0_direction_N_command_L(){
         //given
         MarsRoverPostion marsRoverPostion =new MarsRoverPostion(0,0,"N");
         MarsRover marsRover =new MarsRover(marsRoverPostion);
@@ -14,6 +14,19 @@ public class MarsRoverTest {
         marsRover.receive("L");
         //then
         Assert.assertEquals("W",marsRoverPostion.getDirection());
+        Assert.assertEquals(0,marsRoverPostion.getcoordinateX());
+        Assert.assertEquals(0,marsRoverPostion.getCoordinateY());
+    }
+
+    @Test
+    public void  should_return_coordinate_x0_y0_direction_E_given_coordinate_x0_y0_direction_S_command_L(){
+        //given
+        MarsRoverPostion marsRoverPostion =new MarsRoverPostion(0,0,"S");
+        MarsRover marsRover =new MarsRover(marsRoverPostion);
+        //when
+        marsRover.receive("L");
+        //then
+        Assert.assertEquals("E",marsRoverPostion.getDirection());
         Assert.assertEquals(0,marsRoverPostion.getcoordinateX());
         Assert.assertEquals(0,marsRoverPostion.getCoordinateY());
     }
