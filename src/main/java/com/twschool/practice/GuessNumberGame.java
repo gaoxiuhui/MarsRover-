6dd84972c57fe7d6005d8e110ad04699d310298b
luna <1661639144@qq.com>
2020-06-19 20:14:21 +0800

    luna finish 重构GuessNumberGame代码

--- src/main/java/com/twschool/practice/GuessNumberGame.java
+++ src/main/java/com/twschool/practice/GuessNumberGame.java
@@ -2,10 +2,10 @@ package com.twschool.practice;
 
 public class GuessNumberGame {
 
-
+    public static final int MAX_TRY_TIMES=6;
     private GuessNumberAnswer guessNumberAnswer;
-    private GameStatus gameStatus;
-    int leftTryTime;
+    private GameStatus gameStatus=GameStatus.CONTINUE;
+    private int leftTryTime=MAX_TRY_TIMES;
 
     public GuessNumberGame(GuessNumberAnswer guessNumberAnswer) {
 
@@ -14,15 +14,23 @@ public class GuessNumberGame {
 
     public String guess(String userAnswerString) {
         String result= guessNumberAnswer.check(userAnswerString);
-        leftTryTime=0;
+        decreaseTryTimes();
+        modifyStatus(result);
+        return result;
+    }
+
+    private void modifyStatus(String result) {
         if("4A0B".equals(result)){
-            gameStatus=GameStatus.SUCCESS;
+            gameStatus= GameStatus.SUCCESS;
         } else if(leftTryTime ==0){
             gameStatus=GameStatus.FAILED;
         }else{
             gameStatus=GameStatus.CONTINUE;
         }
-          return result;
+    }
+
+    private void decreaseTryTimes() {
+        leftTryTime--;
     }
 
     public GameStatus getStatus() {
