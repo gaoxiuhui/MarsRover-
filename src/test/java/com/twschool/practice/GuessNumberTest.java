fcfc050ad8036ef68b42ff4299af1f9c94525983
luna <1661639144@qq.com>
2020-06-19 16:34:19 +0800

    luna finish should_return_1_A_1_B_when_check_given_number_part_corr_position_part_corr

--- src/test/java/com/twschool/practice/GuessNumberTest.java
+++ src/test/java/com/twschool/practice/GuessNumberTest.java
@@ -27,4 +27,15 @@ public class GuessNumberTest {
         Assert.assertEquals("1A3B",result);
     }
 
+    @Test
+    public void should_return_1_A_1_B_when_check_given_number_part_corr_position_part_corr(){
+        //given
+        int[] gameAnswer ={1,2,3,4};
+        int[] userAnswer ={1,3,7,8};
+        //when
+        String  result=Answer.check(gameAnswer,userAnswer);
+        //then
+        Assert.assertEquals("1A1B",result);
+    }
+
 }
