d0968903e8eaca4124df4d868f4742f679a743a8
luna <1661639144@qq.com>
2020-06-19 16:31:59 +0800

    luna finish should_return_1_A_3_B_when_check_given_number_all_corr_position_part_corr

--- src/main/java/com/twschool/practice/Answer.java
+++ src/main/java/com/twschool/practice/Answer.java
@@ -3,7 +3,30 @@ package com.twschool.practice;
 public class Answer {
 
     public static String check(int[] gameAnswer,int[] userAnswer){
-        return "4A0B";
+         int Anum=getANumber(gameAnswer,userAnswer);
+         int Bnum=getBNumber(gameAnswer,userAnswer);
+         return ""+Anum+"A"+Bnum+"B";
+    }
+    //获得A前面的数字
+    private static int getANumber(int[] gameAnswer,int[] userAnswer){
+        int ANumber =0;
+        for(int i=0;i<gameAnswer.length;i++){
+            if(gameAnswer[i]==userAnswer[i])
+                ANumber++;
+        }
+        return ANumber;
+    }
 
+    //获得B前面的数字
+    private static int getBNumber(int[] gameAnswer,int[] userAnswer){
+        int BNumber =0;
+        for(int i=0;i<gameAnswer.length;i++){
+            for(int j=0;j<userAnswer.length;j++){
+                if(i==j) continue;
+                if(gameAnswer[i]==userAnswer[j])
+                    BNumber++;
+            }
+        }
+        return BNumber;
     }
 }
--- src/test/java/com/twschool/practice/GuessNumberTest.java
+++ src/test/java/com/twschool/practice/GuessNumberTest.java
@@ -15,4 +15,16 @@ public class GuessNumberTest {
         //then
         Assert.assertEquals("4A0B",result);
     }
+
+    @Test
+    public void should_return_1_A_3_B_when_check_given_number_all_corr_position_part_corr(){
+        //given
+        int[] gameAnswer ={1,2,3,4};
+        int[] userAnswer ={1,3,4,2};
+        //when
+        String  result=Answer.check(gameAnswer,userAnswer);
+        //then
+        Assert.assertEquals("1A3B",result);
+    }
+
 }
