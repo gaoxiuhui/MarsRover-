b7a0e36d94f0089d517a48c115c20ae05e8d7675
luna <1661639144@qq.com>
2020-06-19 20:52:17 +0800

    luna finish should_return_no_equal_number_when_given_length_4

--- src/test/java/com/twschool/practice/GuessNumberGeneratorTest.java
+++ src/test/java/com/twschool/practice/GuessNumberGeneratorTest.java
@@ -35,5 +35,25 @@ public class GuessNumberGeneratorTest {
             Assert.assertEquals(true,result);
         }
     }
+
+    @Test
+    public void should_return_no_equal_number_when_given_length_4(){
+        //given
+        GuessNumberGenerator  guessNumberGenerator=new GuessNumberGenerator();
+        boolean result=true;
+        //when
+        String gameNumber = guessNumberGenerator.generateNumber(4);
+        //then
+        for(int i=0;i<gameNumber.length();i++){
+            for(int j=i+1;j<gameNumber.length();j++){
+                int numi=gameNumber.charAt(i)-'0';
+                int numj=gameNumber.charAt(j)-'0';
+                if(numi==numj){
+                    result= false;
+                }
+                Assert.assertEquals(true,result);
+            }
+        }
+    }
 }
 
