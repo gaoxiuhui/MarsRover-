32fc43003faa789864ed984ec243f717ff6d579c
luna <1661639144@qq.com>
2020-06-19 19:50:56 +0800

    luna finish -m should_return_2A0B_when_userAnswerString_1256_given_game_answerString_1234

--- src/test/java/com/twschool/practice/GuessNumberGameTest.java
+++ src/test/java/com/twschool/practice/GuessNumberGameTest.java
@@ -15,4 +15,15 @@ public class GuessNumberGameTest {
         Assert.assertEquals("4A0B",result);
     }
 
+    @Test
+    public void should_return_2A0B_when_userAnswerString_1256_given_game_answerString_1234(){
+        //given
+        GuessNumberAnswer  guessNumberAnswer =new GuessNumberAnswer("1 2 3 4");
+        GuessNumberGame guessNumberGame =new GuessNumberGame(guessNumberAnswer);
+        //when
+        String result=guessNumberGame.guess("1 2 5 6");
+        //then
+        Assert.assertEquals("2A0B",result);
+    }
+
 }
